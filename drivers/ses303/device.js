5ea15b6e68bdd2eb3bd32f41b2817fb4321acc93
hank <henrik@hankolsen.com>
2021-03-02 00:52:19 +0100

    Fix require issue

--- drivers/ses303/device.js
+++ drivers/ses303/device.js
@@ -1,7 +1,16 @@
 'use strict';
 
-const { TemperatureSensor } = require('../../lib/TemperatureSensor');
+const { ZwaveDevice } = require('homey-zwavedriver');
 
-class SES303Device extends TemperatureSensor {}
+class SES303Device extends ZwaveDevice {
+
+  async onNodeInit() {
+    this.log('Device Init');
+    this.registerCapability('measure_battery', 'BATTERY');
+    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+    this.registerCapability('measure_humidity', 'SENSOR_MULTILEVEL');
+  }
+
+}
 
 module.exports = SES303Device;
--- drivers/srt321/device.js
+++ drivers/srt321/device.js
@@ -1,7 +1,19 @@
 'use strict';
 
-const { ThermostatDevice } = require('../../lib/ThermostatDevice');
+const { ZwaveDevice } = require('homey-zwavedriver');
 
-class SRT321Device extends ThermostatDevice {}
+class SRT321Device extends ZwaveDevice {
+
+  async onNodeInit() {
+    this.log('Device Init');
+    this.registerCapability('measure_battery', 'BATTERY');
+    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+    this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
+
+    // Enable temperature sensor by default
+    await this.configurationSet({ index: 1, size: 1, signed: false }, 255);
+  }
+
+}
 
 module.exports = SRT321Device;
