2ecaf9e82309a078ecf2156abe3216364b5a5c36
hank <henrik@hankolsen.com>
2021-03-05 22:40:33 +0100

    Add more logging. Update SRT321 learnmode.

--- drivers/srt323/device.js
+++ drivers/srt323/device.js
@@ -5,7 +5,7 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 class SRT323Device extends ZwaveDevice {
 
   async onNodeInit() {
-    this.log('Device Init');
+    this.log('SRT323Device Init');
     this.registerCapability('measure_battery', 'BATTERY');
     this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
     this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
