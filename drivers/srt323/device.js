a28995b9875802175038ca8518e8ebbe6bd84d28
hank <henrik@hankolsen.com>
2021-03-27 20:11:33 +0100

    Use getOnOnline for battery devices

--- drivers/srt323/device.js
+++ drivers/srt323/device.js
@@ -5,12 +5,20 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 class SRT323Device extends ZwaveDevice {
 
   async onNodeInit() {
-    this.enableDebug();
     this.log('SRT323Device Init');
-    this.printNodeSummary();
     this.registerCapability('measure_battery', 'BATTERY');
-    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
-    this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT');
+    this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL', {
+      getOpts: {
+        getOnOnline: true,
+        getOnStart: false,
+      },
+    });
+    this.registerCapability('target_temperature', 'THERMOSTAT_SETPOINT', {
+      getOpts: {
+        getOnOnline: true,
+        getOnStart: false,
+      },
+    });
 
     // Enable temperature sensor by default
     await this.configurationSet({ index: 1, size: 1, signed: false }, 255);
