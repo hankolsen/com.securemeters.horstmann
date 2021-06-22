ae4542c1ca5e88c3f173377301ab4e9b24116058
Hank <hank@hank.se>
2021-06-22 17:36:16 +0200

    Add support for SSR-302

---
+++ drivers/ssr302/device.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { ZwaveDevice } = require('homey-zwavedriver');
+
+class SSR303Device extends ZwaveDevice {
+  async onNodeInit() {
+    this.log('SSR302Device Init');
+    this.printNodeSummary();
+
+    this.registerCapability('onoff', 'SWITCH_BINARY', {
+      setParser: (value) => ({
+        'Switch Value': value > 0 ? 255 : 0,
+      }),
+      reportParser: (report) => report.Value === 'on/enable',
+      setParserV1: (value) => ({
+        'Switch Value': value > 0 ? 255 : 0,
+      }),
+      reportParserV1: (report) => report.Value === 'on/enable',
+    });
+  }
+}
+
+module.exports = SSR303Device;
